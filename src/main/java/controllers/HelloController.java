ea94822e1388ea35b9433b97c884c8f5466ddc8a
Roman Gavryshkiv <rgavrysh@softserveinc.com>
2017-07-05 16:47:52 +0200

    added uuid to hello page

--- src/main/java/controllers/HelloController.java
+++ src/main/java/controllers/HelloController.java
@@ -6,11 +6,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.UUID;
+
 @Controller
 public class HelloController {
 
     @RequestMapping(value = "/{name}", method = RequestMethod.GET)
     public ModelAndView helloUser(@PathVariable(value = "name") String name) {
-        return new ModelAndView("hello", "msg", name);
+        String id = UUID.randomUUID().toString();
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("hello");
+        modelAndView.addObject("user", name);
+        modelAndView.addObject("id", id);
+        return modelAndView;
     }
 }
--- src/main/webapp/WEB-INF/views/hello.jsp
+++ src/main/webapp/WEB-INF/views/hello.jsp
@@ -1,6 +1,7 @@
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <html>
 <body>
-<h2>Hello <c:out value="${msg}" default="user"/>!</h2>
+<h2>Hello ${user}!</h2>
+<p>Your ID : ${id}</p>
 </body>
 </html>
